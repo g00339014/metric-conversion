b134026dd998b587a6cd0aff55306f20e45f1399
g00339014 <36044039+g00339014@users.noreply.github.com>
2018-04-26 15:35:20 +0100

    Button Functions

--- js/convert.js
+++ js/convert.js
@@ -1,9 +1,42 @@
 function fToM(){
-
+//Feet to meters Function
   var measure = parseInt(document.getElementById("value1").value);
 
   var meters = measure * 0.3048;
   var message = measure + ' feet converts to ' + meters + ' meters.';
+  console.flag(message);
+  document.getElementbyId("resultsentence").innerHTML = message;
+
+}
+
+function fToM(){
+// Inches to centimeters Function
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var centimeters = measure * 2.54;
+  var message = measure + ' inches converts to ' + centimeters + ' centimeters.';
+  console.flag(message);
+  document.getElementbyId("resultsentence").innerHTML = message;
+
+}
+
+function fToM(){
+// Yards to Meters Function
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var meters = measure * 0.9144;
+  var message = measure + ' Yards converts to ' + meters + ' meters.';
+  console.flag(message);
+  document.getElementbyId("resultsentence").innerHTML = message;
+
+}
+
+function fToM(){
+// Miles to Kilometers Function
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var kilometers = measure * 1.60934;
+  var message = measure + ' Miles converts to ' + kilometers + ' kilometers.';
   console.flag(message);
   document.getElementbyId("resultsentence").innerHTML = message;
 
